1043b4e2159a28bd3f630f99403bb5184a9edb99
JeremyS-T <jtanrev@gmail.com>
2024-03-03 15:28:39 +0800

    Add core Krita plugin connection

--- music_art_gen.py
+++ music_art_gen.py
@@ -1,5 +1,5 @@
 from krita import *
-from music_gen import *
 
-def trigger_my_plugin():
-    Krita.instance().action("music_art_gen").trigger()
+def trigger_plugin():
+    doc = Krita.instance().createDocument(1000, 1000, "Music_Gen", "RGBA", "U8", "", 120.0)
+    Krita.instance().activeWindow().addView(doc)
